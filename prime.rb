451db0c73728a7c100015b7354f98ef469c3d1e6
Cully Curwen <c.t.g.curwen@gmail.com>
2019-03-03 17:43:54 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+def prime?(num)
+  if num <= 1
+    return false
+  end
+
+  range = (2..num-1).to_a
+
+  range.each do |test|
+    if num % test == 0
+      return false
+    end
+  end
+  true
+end
