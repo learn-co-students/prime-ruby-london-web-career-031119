1d9c3c445e106440a07920f54f2f639fae628cc4
github username <github email address>
2019-03-04 12:36:27 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
+def prime?(num)
+  arr = (2..num-1).to_a
+  if num <= 1
+    return false
+  end
+  arr.each do |test|
+    if num % test == 0
+      return false
+    end
+  end
+  return true
+end
+
+
+
 # Add  code here!
