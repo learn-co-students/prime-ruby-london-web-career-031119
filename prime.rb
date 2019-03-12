666a5296fbf48a050d7a27a9a290be4f6e6637f7
Vasile Cojusco <kozhushko.vasyl@gmail.com>
2019-03-12 11:18:56 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -2,13 +2,10 @@
 
 def prime?(num)
   prime = true
-  num_range = (2...num - 1).to_a
   num < 2 && (prime = false)
 
-  num_range.each do |el|
-    if num % el == 0
-      prime = false
-    end
+  (2...num - 1).to_a.each do |el|
+    num % el == 0 && (prime = false)
   end
 
   prime
